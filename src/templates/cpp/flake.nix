0cf85d686ecb41fbc64666f12cf390291308fe8e
vicgeentor <github@vicgeentor.nl>
2026-04-14 16:29:50 +0200

    added cpp template

---
+++ src/templates/cpp/flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        { pkgs, ... }:
+        {
+          packages.default = pkgs.stdenv.mkDerivation {
+            pname = "CHANGE_ME";
+            version = "0.0.1";
+            src = ./.;
+
+            # Either specify buildPhase and installPhase here or use a Makefile
+            # with build and install instructions.
+            # E.g.,
+            # buildPhase = ''
+            #   make
+            # '';
+            # installPhase = ''
+            #   mkdir -p $out/bin
+            #   cp default_pname $out/bin/
+            # '';
+          };
+          devShells.default =
+            pkgs.mkShell.override
+              {
+                stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages = with pkgs; [
+                  bear # run `bear -- make` to generate compile_commands.json for clangd to work
+                  clang-tools
+                  gnumake
+                ];
+              };
+        };
+    };
+}
