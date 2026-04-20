b6b349e6cc82051a2e07a5a7d3cd8f5ffee058b4
vicgeentor <github@vicgeentor.nl>
2026-04-20 14:05:04 +0200

    haskell: added pre-commit hooks

--- src/templates/haskell/flake.nix
+++ src/templates/haskell/flake.nix
@@ -2,31 +2,49 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+      ];
+
+      systems = [ "x86_64-linux" ];
+
       perSystem =
-        { pkgs, ... }:
-        let
-          pkg = pkgs.haskellPackages.developPackage { root = ./.; };
-        in
+        { config, pkgs, ... }:
         {
           packages = rec {
-            CHANGE_ME = pkg;
+            CHANGE_ME = pkgs.haskellPackages.developPackage { root = ./.; };
             default = CHANGE_ME;
           };
+
           devShells.default = pkgs.haskellPackages.shellFor {
-            packages = _: [ pkg ];
+            packages = _: [ config.packages.default ];
             nativeBuildInputs = with pkgs; [
               cabal-install
               ghc
               haskellPackages.cabal-fmt
               haskell-language-server
+              hlint
             ];
+            shellHook = config.pre-commit.shellHook;
+          };
+
+          pre-commit.settings = {
+            hooks = {
+              nixfmt.enable = true;
+              cabal-fmt.enable = true;
+              ormolu.enable = true;
+              hlint.enable = true;
+            };
           };
         };
     };
