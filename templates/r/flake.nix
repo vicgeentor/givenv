18294c0445f396b3d7b54124a486353da3e01c3d
vicgeentor <github@vicgeentor.nl>
2026-06-26 15:01:16 +0200

    fix: r flake

--- templates/r/flake.nix
+++ templates/r/flake.nix
@@ -11,27 +11,22 @@
       systems = import inputs.systems;
       perSystem =
         { pkgs, ... }:
-        {
-          overlays.default = final: prev: {
-            rEnv = final.rWrapper.override {
-              packages = with final.rPackages; [
-                rmarkdown
-                tidyverse
-                ggplot2
-                ggpubr
-                languageserver
-
-                # Required for grind.R by Rob de Boer
-                coda
-                deSolve
-                rootSolve
-                FME
-              ];
-            };
+        let
+          R-with-packages = pkgs.rWrapper.override {
+            packages = with pkgs.rPackages; [
+              rmarkdown
+              tidyverse
+              ggplot2
+              ggpubr
+              languageserver
+            ];
           };
+        in
+
+        {
 
           devShells.default = pkgs.mkShell {
-            packages = [ pkgs.rEnv ];
+            packages = [ R-with-packages ];
           };
         };
     };
